e31f721d8bdc8ac1c40f1b82561ce2a1377e3cb5
venkatlakshmirangasamy <venkatlakshmi.rcse@ksrce.ac.in>
2016-07-21 13:42:21 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Vowels {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter your character:");
+		char u=scanner.next().charAt(0);
+		switch(u)
+		{
+		case 'a':
+			System.out.println("it is one of the vowels");
+			break;
+		case 'e':
+			System.out.println("it is one of the vowels");
+			break;
+		case 'i':
+			System.out.println("it is one of the vowels");
+			break;
+		case 'o':
+			System.out.println("it is one of the vowels");
+			break;
+		case 'u':
+			System.out.println("it is one of the vowels");
+			break;
+			default:
+				System.out.println("not vowels");
+				break;
+		}
+		
+	}
+
+}
