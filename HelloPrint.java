0da0ade16e06b31071db88a951e8033ac7b64c4b
venkatlakshmirangasamy <venkatlakshmi.rcse@ksrce.ac.in>
2016-07-30 17:03:24 +0530

    Create HelloPrint.java

---
+++ HelloPrint.java
@@ -0,0 +1,11 @@
+public class HelloPrint {
+public static void main(String[] args) {
+	String st="Hello";
+	String st1="";
+	for(int i=0;i<5;i++)
+	{
+		st1+=st;
+	}
+	System.out.println(st1);
+}
+}
