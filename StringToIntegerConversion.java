ee4c2071e8799876b43b20c0cd826737e7c34351
venkatlakshmirangasamy <venkatlakshmi.rcse@ksrce.ac.in>
2016-09-02 11:13:34 +0530

    Create StringToIntegerConversion.java

---
+++ StringToIntegerConversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringToIntegerConversion {
+public static void main(String[] args) {
+String str="java";
+StringBuffer sr=new StringBuffer(str);
+for(int i=0;i<sr.length();i++){
+	int c=sr.charAt(i)-96;
+	System.out.println(c);
+}
+
+}
+}
